10fc816059955caf446e98581090a2b46c1043fe
Andrey Shemet <an-shem@ukr.net>
2021-04-22 01:45:18 +0300

    Сделал 3-е задание

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import user from './data/user.json';
-import Profile from './сomponents/task-1/Profile/Profile.jsx';
+import friends from './data/friends.json';
 import statisticalData from './data/statistical-data.json';
+
+import Profile from './сomponents/task-1/Profile/Profile.jsx';
 import Statistics from './сomponents/task-2/Statistics/Statistics.jsx';
 import Container from './сomponents/Container/Container';
+import FriendList from './сomponents/task-3/FriendList/FriendList';
 
 function App() {
   return (
@@ -15,8 +18,18 @@ function App() {
           avatar={user.avatar}
           stats={user.stats}
         />
+        <br />
+        <br />
+        <br />
         <Statistics title="Upload stats" stats={statisticalData} />;
         <Statistics stats={statisticalData} />;
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <FriendList friends={friends} />
       </Container>
     </div>
   );
---
+++ src/сomponents/task-3/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,13 @@
+import styles from './FriendListItem.module.css';
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className={styles.item}>
+      <span className={isOnline ? styles.online : styles.offline}></span>
+      <img className={styles.avatar} src={avatar} alt={name} width="48" />
+      <p className={styles.name}>{name}</p>
+    </li>
+  );
+};
+
+export default FriendListItem;
